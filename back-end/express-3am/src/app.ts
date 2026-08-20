60744fc935a291a00436dc5294b09c37aa760454
JWon <jwon1818@daum.net>
2026-08-20 19:36:23 +0900

    [SET] prettier

--- back-end/express-3am/src/app.ts
+++ back-end/express-3am/src/app.ts
@@ -3,9 +3,9 @@ import express, { type Express, type Request, type Response } from 'express';
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+	console.log('Server is running on port 3000');
 });
--- back-end/express-3am/src/repositories/memger.ts
+++ back-end/express-3am/src/repositories/memger.ts
@@ -2,7 +2,7 @@ import { db } from '../db/index.ts';
 import { member } from '../db/schema/member.ts';
 
 export const memberRepository = {
-    findAll: () => {
-        return db.select().from(member);
-    }
-}
+	findAll: () => {
+		return db.select().from(member);
+	},
+};
