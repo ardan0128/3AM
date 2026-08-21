d8a198f0fe54764211f02d58761393b7aaf49dc0
JWon <jwon1818@daum.net>
2026-08-21 14:47:19 +0900

    [FIX] Team & Member entity

--- back-end/springboot-3am/src/main/java/com/ardan/springboot_3am/entity/Team.java
+++ back-end/springboot-3am/src/main/java/com/ardan/springboot_3am/entity/Team.java
@@ -1,6 +1,7 @@
 package com.ardan.springboot_3am.entity;
 
 import jakarta.persistence.*;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,23 +11,25 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@Entity
+@Entity(name = "teams")
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Team {
 
     @Id
-    @Column(name = "id", nullable = false)
-    private Long id;
+    @Column(name = "id", nullable = false, columnDefinition = "uuid")
+    private UUID id;
 
     @Column(name = "name", nullable = false)
     private String name;
 
     @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
     @ManyToOne
