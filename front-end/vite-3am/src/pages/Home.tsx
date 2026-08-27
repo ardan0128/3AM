bfa636784d69ea8c519b11e296725e47ec5bf929
JWon <jwon1818@daum.net>
2026-08-27 19:28:09 +0900

    [SET] axios

--- front-end/vite-3am/src/pages/Home.tsx
+++ front-end/vite-3am/src/pages/Home.tsx
@@ -1,8 +1,38 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+type Post = {
+  id: number;
+  title: string;
+  userId: number;
+  body: string;
+};
+
 export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Post[]>(`https://jsonplaceholder.typicode.com/posts`)
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch((error) => {
+        console.error(`Error fetching data`, error);
+      });
+  }, []);
+
   return (
     <>
       <h1>page: Home</h1>
       <a>Test</a>
+      {posts.map((post) => {
+        return (
+          <div className="text-white" key={post.id}>
+            {post.title}
+          </div>
+        );
+      })}
     </>
   );
 }
