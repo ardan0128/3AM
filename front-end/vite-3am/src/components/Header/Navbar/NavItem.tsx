d66c95bb771a8d6122e3f19598de8fa81e8371ff
JWon <jwon1818@daum.net>
2026-08-27 15:53:47 +0900

    [FEAT] add active css at NavLink

--- front-end/vite-3am/src/components/Header/Navbar/NavItem.tsx
+++ front-end/vite-3am/src/components/Header/Navbar/NavItem.tsx
@@ -1,3 +1,4 @@
+import styles from './NavItem.module.css';
 import { NavLink } from 'react-router';
 
 interface NavItemProps {
@@ -9,7 +10,9 @@ export default function NavItem({ to, children }: NavItemProps) {
   return (
     <>
       <NavLink
-        className="relative text-gray-400 after:absolute after:right-0 after:-bottom-2 after:h-0.5 after:w-0 after:bg-gray-200 after:transition-all after:duration-300 after:content-[''] hover:text-gray-200 hover:after:left-0 hover:after:w-full"
+        className={({ isActive }) =>
+          `${styles.navItem} ${isActive ? styles.active : ''}`
+        }
         to={to}
       >
         {children}
