a91f1642efff562b71767e85aaef145836134d88
JWon <jwon1818@daum.net>
2026-08-28 15:11:12 +0900

    [FEAT] update schema member

--- back-end/express-3am/src/db/schema/member.ts
+++ back-end/express-3am/src/db/schema/member.ts
@@ -7,6 +7,13 @@ export const member = pgTable('members', {
     .default(sql`uuidv7()`)
     .primaryKey(),
   name: varchar('name', { length: 255 }).notNull(),
+  mainColor: varchar('main_color', { length: 7 }),
+  subColor: varchar('sub_color', { length: 7 }),
+  chzzkId: varchar('chzzk_id', { length: 255 }),
+  youtubeHandle: varchar('youtube_handle', { length: 255 }),
+  naverCafe: varchar('naver_cafe', { length: 255 }),
+  xId: varchar('x_id', { length: 255 }),
+
   teamId: uuid('team_id').references(() => team.id),
   createdAt: timestamp('created_at', { withTimezone: true })
     .defaultNow()
