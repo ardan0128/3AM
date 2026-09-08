6b7ca4fba57137d1972370fb00328cb7338de93b
JWon <jwon1818@daum.net>
2026-09-08 22:09:39 +0900

    [FEAT] add trigger.sql for member_theme

--- back-end/database/trigger.sql
+++ back-end/database/trigger.sql
@@ -7,3 +7,8 @@ CREATE TRIGGER trg_teams_updated_at
     BEFORE UPDATE ON teams
     FOR EACH ROW
 EXECUTE FUNCTION update_updated_at();
+
+CREATE TRIGGER trg_member_theme_updated_at
+    BEFORE UPDATE ON member_themes
+    FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
