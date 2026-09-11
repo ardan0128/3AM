9ad8932db8829d26daa99d85251f62b4ab580082
JWon <jwon1818@daum.net>
2026-09-11 16:28:22 +0900

    [FEAT] link button

--- front-end/vite-3am/src/components/Home/Card/CardFront.tsx
+++ front-end/vite-3am/src/components/Home/Card/CardFront.tsx
@@ -1,15 +1,16 @@
-import { ImageOff } from 'lucide-react';
+import { ExternalLink, ImageOff } from 'lucide-react';
 import type { MemberProps } from '../types';
 
 export default function CardFront({ member }: { member: MemberProps }) {
   return (
     <>
       <div
-        className="flex h-full flex-col rounded-xl border-2 text-white"
+        className="flex h-full flex-col overflow-hidden rounded-xl border-2 text-white"
         style={{ borderColor: `${member.mainColor}` }}
         key={member.id}
       >
         <div className="p-2 text-center text-2xl font-bold">{member.name}</div>
+
         <div
           className="shrink-0"
           style={{ backgroundColor: `${member.mainColor}` }}
@@ -24,37 +25,70 @@ export default function CardFront({ member }: { member: MemberProps }) {
             <ImageOff className="block aspect-square h-full w-full object-cover" />
           )}
         </div>
-        <div className="flex min-h-0 flex-1 flex-col p-4">
-          <div className="flex flex-1 items-center">
-            <a href={member.chzzkId} target="_blank" rel="noopener noreferrer">
-              calendar
-            </a>
+
+        <div className="flex min-h-0 flex-1 flex-col">
+          <div
+            className="relative flex flex-1 items-center justify-between px-2"
+            style={{
+              backgroundImage: `linear-gradient(to right top, #ABA089, #7F755F)`,
+            }}
+          >
+            <span>calendar</span>
+            <ExternalLink />
+            <a
+              href={member.chzzkId}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="absolute inset-0"
+            />
           </div>
-          <div className="flex flex-1 items-center">
+
+          <div
+            className="relative flex flex-1 items-center justify-between px-2"
+            style={{
+              backgroundImage: `linear-gradient(to right top, #ABA089, #7F755F)`,
+            }}
+          >
+            <span>chzzk</span>
+            <ExternalLink />
             <a
               href={`${import.meta.env.VITE_CHZZK_URL}${member.chzzkId}`}
               target="_blank"
-            >
-              chzzk
-            </a>
+              rel="noopener noreferrer"
+              className="absolute inset-0"
+            />
           </div>
-          <div className="flex flex-1 items-center">
+
+          <div
+            className="relative flex flex-1 items-center justify-between px-2"
+            style={{
+              backgroundImage: `linear-gradient(to right top, #ABA089, #7F755F)`,
+            }}
+          >
+            <span>cafe</span>
+            <ExternalLink />
             <a
               href={`${import.meta.env.VITE_CAFE_URL}${member.naverCafe}`}
               target="_blank"
               rel="noopener noreferrer"
-            >
-              cafe
-            </a>
+              className="absolute inset-0"
+            />
           </div>
-          <div className="flex flex-1 items-center">
+
+          <div
+            className="relative flex flex-1 items-center justify-between px-2"
+            style={{
+              backgroundImage: `linear-gradient(to right top, #ABA089, #7F755F)`,
+            }}
+          >
+            <span>youtube</span>
+            <ExternalLink />
             <a
               href={`${import.meta.env.VITE_YOUTUBE_URL}${member.youtubeHandle}`}
               target="_blank"
               rel="noopener noreferrer"
-            >
-              youtube
-            </a>
+              className="absolute inset-0"
+            />
           </div>
         </div>
       </div>
