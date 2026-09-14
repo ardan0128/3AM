f0f9106c0e26c327bddc94b1f55aea2621ba87a6
JWon <jwon1818@daum.net>
2026-09-14 14:53:34 +0900

    [FEAT] delete api get by team

--- back-end/express-3am/src/modules/member/route.ts
+++ back-end/express-3am/src/modules/member/route.ts
@@ -7,7 +7,6 @@ import {
   updateAll,
   updateOne,
 } from './controller.ts';
-import { getMembersByTeamId } from './controller.ts';
 
 const router = Router();
 
@@ -15,7 +14,6 @@ router.post('/member', createOne);
 router.patch('/member/:id', updateOne);
 router.post('/member/:id/theme', createTheme);
 
-router.get('/members/:teamId', getMembersByTeamId);
 router.get('/members', getMembersAll);
 router.post('/members', createAll);
 router.put('/members', updateAll);
