af2881dcf48e3b6beacdb7d14115bdb3cfb087dd
JWon <jwon1818@daum.net>
2026-09-14 16:44:40 +0900

    [FEAT] footer description

--- front-end/vite-3am/src/components/Footer/Footer.tsx
+++ front-end/vite-3am/src/components/Footer/Footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
     <>
-      <footer className="bg-gray-800 p-4 text-white">footer</footer>
+      <footer className="bg-gray-950 p-4 text-gray-500">
+        본 사이트는 개인 포트폴리오 목적으로 제작되었으며, 상업적으로 이용하지
+        않습니다. 웹사이트 내 포함된 일부 리소스의 저작권은 원저작권자에게
+        있습니다.
+      </footer>
     </>
   );
 }
